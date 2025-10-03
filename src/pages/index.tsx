b9e1b649864f3ef70498107dd999be7c72865f78
shashankMaheshwari23 <65753185+shashankMaheshwari23@users.noreply.github.com>
2025-10-03 12:25:24 +0530

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,19 @@ import { HomeProps } from 'balloon/Views/type'
 
 import { GetServerSideProps } from 'next'
 export const getServerSideProps : GetServerSideProps = async () =>{
-  const response = await request.get('public-products-list?pageSize=10&pageNumber=1');
-  const productList = response.data || []
+  // const response = await request.get('public-products-list?pageSize=10&pageNumber=1');
+  // const productList = response.data || []
   return {
     props: {
-      productList
+      
     }
   }
 }
 
-const Home: React.FC<HomeProps> = ({productList}) => {
+const Home: React.FC<HomeProps> = () => {
   return (
     <>
-        <HomeView productList={productList}></HomeView>
+        <HomeView productList={[]}></HomeView>
     </>
   )
 }
